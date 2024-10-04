03d1335a6dad8480dd79f2bbfdb7068dfaf68ab2
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-10-04 11:02:52 -0300

    created goalsCompleted seed

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,14 +1,25 @@
 import { client, db } from './index';
 import { goals, goalsCompleted } from './schema';
+import dayjs from 'dayjs';
 
 async function seed() {
   await db.delete(goalsCompleted);
   await db.delete(goals);
 
-  await db.insert(goals).values([
-    { title: 'Estudar 3h', desiredWeeklyFrequency: 5 },
-    { title: "Tomar 3L d'água", desiredWeeklyFrequency: 7 },
-    { title: 'Caminhar 5km', desiredWeeklyFrequency: 3 },
+  const response = await db
+    .insert(goals)
+    .values([
+      { title: 'Estudar 3h', desiredWeeklyFrequency: 5 },
+      { title: "Tomar 3L d'água", desiredWeeklyFrequency: 7 },
+      { title: 'Caminhar 5km', desiredWeeklyFrequency: 3 },
+    ])
+    .returning();
+
+  const startOfWeek = dayjs().startOf('week');
+
+  await db.insert(goalsCompleted).values([
+    { goalId: response[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: response[2].id, createdAt: startOfWeek.add(2, 'day').toDate() },
   ]);
 }
 
