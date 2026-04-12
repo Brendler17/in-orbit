0da96cc0e85f77bdbfdb1812def84595b7a78a9b
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2026-04-12 17:53:16 -0300

    created empty screen

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,3 +1,21 @@
+import { Plus } from 'lucide-react';
+import letsStart from './assets/lets-start.svg';
+import logo from './assets/logo-in-orbit.svg';
+import { Button } from './components/ui/button';
+
 export function App() {
-	return <h1>Hello World</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="logo-image" />
+      <img src={letsStart} alt="lets-start-image" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar Meta
+      </Button>
+    </div>
+  );
 }
